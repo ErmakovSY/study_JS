064952ef1cb8e3b415f65cd0dd67190a685bacd6
Sergey <e@gmail.com>
2017-11-11 11:45:24 +0200

    12.11.17

--- present-gulp-react/src/components/map/index.js
+++ present-gulp-react/src/components/map/index.js
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const Map = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBb7wZFJHwk6nWfeMdQy4fMPfIelzwdSpU&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `350px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
